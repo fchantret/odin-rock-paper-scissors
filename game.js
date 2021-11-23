20aa127b20f8dd4e4f97d9a3932381e3e9f29da1
Florent CHANTRET <florent@chantret.com>
2021-11-24 00:21:55 +0100

    Add playRound() function

--- game.js
+++ game.js
@@ -11,3 +11,45 @@ function computerPlay() {
             return 'Scissors';
     };
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let win = null;
+    let tie = false;
+
+    if (playerSelection === 'Rock') {
+        if (computerSelection === 'Rock') {
+            tie = true;
+        } else if (computerSelection === 'Paper') {
+            win = false;
+        } else {
+            win = true;
+        }
+    } else if (playerSelection === 'Paper') {
+        if (computerSelection === 'Rock') {
+            win = true;
+        } else if (computerSelection === 'Paper') {
+            tie = true;
+        } else {
+            win = false;
+        }
+    } else if (playerSelection === 'Scissors') {
+        if (computerSelection === 'Rock') {
+            win = false;
+        } else if (computerSelection === 'Paper') {
+            win = true;
+        } else {
+            tie = true;
+        }
+    } else {
+        return 'Error: playerSelection invalid!';
+    }
+
+    if (tie) {
+        return `It's a tie! You both choosed ${playerSelection}`;
+    } else if (win) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
